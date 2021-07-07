0c24db2ff9d00d6dc4671278c7973918dac9c1dd
EvgeniiRuchko <gk1987@mail.ru>
2021-07-07 21:44:54 +0300

    Создан проект для ДЗ7

---
+++ HomeWork-7/src/main/java/ru/geekbrains/app/homework7/HomeWork7Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.app.homework7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork7Application.class, args);
+    }
+
+}
