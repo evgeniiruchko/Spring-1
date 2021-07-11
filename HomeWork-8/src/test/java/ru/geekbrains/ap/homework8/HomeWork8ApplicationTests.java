752343c17813f87c5c44e53811c72e6de2aba97a
EvgeniiRuchko <gk1987@mail.ru>
2021-07-11 11:29:38 +0300

    HomeWork-8. Initialize

---
+++ HomeWork-8/src/test/java/ru/geekbrains/ap/homework8/HomeWork8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.ap.homework8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
